662c2c6707fc662220bd7af276cf0c01310de2c5
Michael Yin <layerssss@gmail.com>
2012-05-01 23:26:13 +0800

    automatic arguments dumping

--- ispJs/RuntimeScripts/isp_runtime.js
+++ ispJs/RuntimeScripts/isp_runtime.js
@@ -4,5 +4,5 @@
 var $load = function (path) {
     path = String(path);
     $_native_loadedJS += '|' + path;
-    return new Function($_native_loadJS(path));
+    return new Function("try {for (key in arguments[0]) {this[key] = arguments[0][key];}}catch (e) {}"+$_native_loadJS(path));
 };
